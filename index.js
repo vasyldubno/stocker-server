e3d8377255b85d7c54654c7d800c8c749cf8cfac
Vasyl Dubno <vasyldubno@gmail.com>
2023-08-05 12:32:00 -0700

    fix

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.get("/", (req, res) => {
 
 app.get('/update-price-current', async (req, res) => {
   const client = supabase.createClient('https://cufytakzggluwlfdjqsn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1Znl0YWt6Z2dsdXdsZmRqcXNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAzNzAyMDcsImV4cCI6MjAwNTk0NjIwN30.IAvHQ2HBCWaPzq71nK3e9k_h3Cu7VYVMBzCghiaqDl4')
-  res.json({ route: '/update-price-current'})
+
+  const stocks = await client.from('stock').select().in('ticker', ['A', 'AAPL'])
+
+  res.json({ route: '/update-price-current', result: { stocks } })
 })
 
 app.listen(80, () => {
