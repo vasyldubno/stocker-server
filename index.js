58069231bdacc0e1290c7ea7f5852e76ffa2e871
Vasyl Dubno <vasyldubno@gmail.com>
2023-08-05 16:49:48 -0700

    fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const getPriceGrowth = require('./src/utils/getPriceGrowth.js')
 const ROUND = require('./src/utils/round.js')
 const cron = require('cron');
 const { default: axios } = require("axios");
+const moment = require('moment-timezone')
 
 require("dotenv").config();
 
@@ -53,7 +54,7 @@ app.get('/update-price-current', async (req, res) => {
               const priceYearHigh = stock.price_year_high && priceCurrent > stock.price_year_high ? priceCurrent : stock.price_year_high
               const gfValueMargin = ROUND(((Number(stock.gfValue) - priceCurrent) / priceCurrent) * 100)
 
-              const r = await client
+              await client
                 .from('stock')
                 .update({
                   price_current: priceCurrent, 
@@ -140,10 +141,167 @@ app.get('/update-price-current', async (req, res) => {
   res.json({ route: '/update-price-current', result: { stocks } })
 })
 
-const job = new cron.CronJob('*/30 * * * *', async () => {
+app.get('/update-dividends', async (req, res) => {
+  const supabaseClient = supabase.createClient(
+    'https://cufytakzggluwlfdjqsn.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1Znl0YWt6Z2dsdXdsZmRqcXNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAzNzAyMDcsImV4cCI6MjAwNTk0NjIwN30.IAvHQ2HBCWaPzq71nK3e9k_h3Cu7VYVMBzCghiaqDl4'
+  )
+
+  const stocks = await supabaseClient
+    .from("stock")
+    .select()
+    // .eq("is_trading", true)
+    // .eq("is_dividend", true)
+    // .in('ticker', ['A', 'AAPL'])
+    .order("ticker", { ascending: true });
+
+  if (stocks.data) {
+    stocks.data.forEach((stock, index) => {
+      setTimeout(async () => {
+        try {
+          const response = await axios.get(
+            `https://api.polygon.io/v3/reference/dividends?ticker=${stock.ticker}&apiKey=OZ_9x0ccKRsnzoE6OqsoW0oGeQCmAohs`
+          );
+
+          /* --- UPDATE UPCOMING DIVIDEND ---  */
+          const today = moment().format("YYYY-MM-DD");
+
+          const lastUpcomeDividend = response.data.results.find((item) =>
+            moment(item.pay_date).isAfter(today)
+          );
+
+          await supabaseClient
+            .from("stock")
+            .update({
+              dividend_upcoming_date: lastUpcomeDividend?.pay_date
+                ? lastUpcomeDividend?.pay_date
+                : null,
+              dividend_upcoming_value: lastUpcomeDividend?.cash_amount
+                ? lastUpcomeDividend?.cash_amount
+                : null,
+            })
+            .eq("ticker", stock.ticker);
+
+          /* --- UPDATE DIVIDEND INCOME --- */
+          // const supaStockPortfolio = await supabaseClient
+          //   .from("stock_portfolio")
+          //   .select()
+          //   .eq("ticker", stock.ticker);
+
+          // if (supaStockPortfolio.data) {
+          //   supaStockPortfolio.data.forEach(async (stockPortfolio) => {
+          //     if (stockPortfolio.startTradeDate) {
+          //       const today = moment().format("YYYY-MM-DD");
+          //       const std = new Date(stockPortfolio.startTradeDate);
+
+                
+
+          //       const last = response.data.results.find(
+          //         (res) =>
+          //           moment(new Date(res.pay_date)).isBefore(new Date(today)) &&
+          //           moment(new Date(res.ex_dividend_date)).isAfter(moment(std))
+          //       );
+
+          //       if (last) {
+          //         const isExistDividends = moment(
+          //           stockPortfolio.lastDividendPayDate
+          //         ).isSame(moment(last.pay_date));
+
+          //         if (
+          //           !isExistDividends &&
+          //           stockPortfolio.amount_active_shares > 0 &&
+          //           stockPortfolio.average_cost_per_share
+          //         ) {
+          //           const supaPortfolio = await supabaseClient
+          //             .from("portfolio")
+          //             .select()
+          //             .eq("id", stockPortfolio.portfolio_id)
+          //             .single();
+
+          //           if (supaPortfolio.data) {
+          //             const supaUser = await supabaseClient
+          //               .from("user")
+          //               .select()
+          //               .eq("id", supaPortfolio.data.user_id)
+          //               .single();
+
+          //             if (supaUser.data) {
+          //               const totalDividends = ROUND(
+          //                 last.cash_amount * stockPortfolio.amount_active_shares
+          //               );
+          //               const cost =
+          //                 stockPortfolio.average_cost_per_share *
+          //                 stockPortfolio.amount_active_shares;
+          //               const newTotalReturnValue = ROUND(
+          //                 stockPortfolio.total_return_value ? stockPortfolio.total_return_value : 0 + totalDividends
+          //               );
+          //               const newTotalReturnMargin = ROUND(
+          //                 (newTotalReturnValue / cost) * 100
+          //               );
+          //               const newTotalDividendIncome = ROUND(
+          //                 stockPortfolio.total_dividend_income ? stockPortfolio.total_dividend_income : 0 + totalDividends
+          //               );
+          //               const newBalance = ROUND(
+          //                 supaUser.data.balance + totalDividends
+          //               );
+          //               const year = Number(
+          //                 moment(last.pay_date).format("YYYY")
+          //               );
+
+          //               await supabaseClient
+          //                 .from("stock_portfolio")
+          //                 .update({
+          //                   lastDividendPayDate: last.pay_date,
+          //                   total_dividend_income: newTotalDividendIncome,
+          //                   total_return_value: newTotalReturnValue,
+          //                   total_return_margin: newTotalReturnMargin,
+          //                 })
+          //                 .eq("ticker", stockPortfolio.ticker)
+          //                 .eq("portfolio_id", stockPortfolio.portfolio_id);
+
+          //               await supabaseClient.from("dividend").insert({
+          //                 amount_shares: stockPortfolio.amount_active_shares,
+          //                 dividendValue: ROUND(last.cash_amount),
+          //                 dividendYield: ROUND(
+          //                   (last.cash_amount /
+          //                     stockPortfolio.average_cost_per_share) *
+          //                     100
+          //                 ),
+          //                 ticker: stock.ticker,
+          //                 payDate: last.pay_date,
+          //                 totalAmount: totalDividends,
+          //                 portfolio_id: stockPortfolio.portfolio_id,
+          //                 year,
+          //               });
+
+          //               await supabaseClient
+          //                 .from("user")
+          //                 .update({ balance: newBalance })
+          //                 .eq("id", supaUser.data.id);
+          //             }
+          //           }
+          //         }
+          //       }
+          //     }
+          //   });
+          // }
+        } catch {}
+      }, index * 15000);
+    });
+  }
+
+  res.json({ message: "Ok" });
+})
+
+const job30m = new cron.CronJob('*/30 * * * *', async () => {
   await axios.get(`${process.env.CLIENT_URL}/update-price-current`)
 })
-job.start()
+job30m.start()
+
+const jobDay = new cron.CronJob('0 0 * * *', async () => {
+  await axios.get(`${process.env.CLIENT_URL}/update-dividends`)
+})
+jobDay.start()
 
 app.listen(80, () => {
   console.log("SERVER WORK")
