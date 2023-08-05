fc6f6f47cb9dfe0cf06fc125326366354a03fcc5
Vasyl Dubno <vasyldubno@gmail.com>
2023-08-05 11:56:32 -0700

    fix

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const { supabaseClient } = require("./src/config/supabaseClient.js");
 const express = require("express");
 const cors = require("cors");
-const { ROUND } = require("./src/utils/round.js")
-const { getPriceCurrent } = require("./src/utils/getPriceCurrent.js")
-const { getPriceGrowth } = require("./src/utils/getPriceGrowth.js")
-const cron = require('cron')
-const axios = require('axios')
+
 require("dotenv").config();
 
 const app = express();
@@ -18,115 +13,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Ok", route: "/" });
 });
 
-// app.get("/update-price-current", async (req, res) => {
-//   const stocks = await supabaseClient
-//     .from("stock")
-//     .select()
-//     .order("ticker", { ascending: true })
-//     .in("ticker", ["A", "AAL", "AAP", "AAPL"]);
-
-//   if (stocks.data) {
-//     stocks.data.forEach((stock, index) => {
-//       setTimeout(async () => {
-//         try {
-//           // const stockPortfolio = await supabaseClient
-//           //   .from("stock_portfolio")
-//           //   .select()
-//           //   .eq("ticker", stock.ticker)
-//           //   .in("portfolio_id", portfolioIds ?? []);
-
-//           if (stock.ticker && stock.price_target) {
-//             const priceCurrent = await getPriceCurrent(
-//               stock.ticker,
-//               stock.exchange
-//             );
-//             if (priceCurrent) {
-//               const priceGrowth = getPriceGrowth(
-//                 stock.price_target,
-//                 priceCurrent
-//               );
-
-//               await supabaseClient
-//                 .from("stock")
-//                 .update({
-//                   price_current: priceCurrent,
-//                   price_growth: priceGrowth,
-//                   price_year_high:
-//                     stock.price_year_high &&
-//                     priceCurrent > stock.price_year_high
-//                       ? priceCurrent
-//                       : stock.price_year_high,
-//                   gfValueMargin: ROUND(
-//                     ((Number(stock.gfValue) - priceCurrent) / priceCurrent) *
-//                       100
-//                   ),
-//                 })
-//                 .eq("ticker", stock.ticker);
-
-//               // if (stockPortfolio.data) {
-//               //   stockPortfolio.data.forEach(async (item) => {
-//               //     if (
-//               //       item.amount_active_shares &&
-//               //       item.average_cost_per_share
-//               //     ) {
-//               //       const portfolio = await supabaseClient
-//               //         .from("portfolio")
-//               //         .select()
-//               //         .eq("id", item.portfolio_id)
-//               //         .single();
-//               //       const value = ROUND(
-//               //         priceCurrent * item.amount_active_shares
-//               //       );
-//               //       const cost = ROUND(
-//               //         item.average_cost_per_share * item.amount_active_shares
-//               //       );
-//               //       const portfolioValue = ROUND(
-//               //         await getPortfolioValue(item.portfolio_id)
-//               //       );
-//               //       await supabaseClient
-//               //         .from("stock_portfolio")
-//               //         .update({
-//               //           price_current: priceCurrent,
-//               //           price_growth: priceGrowth,
-//               //           gain_value: ROUND(value - cost),
-//               //           gain_margin: ROUND(((value - cost) / cost) * 100),
-//               //           perc_of_portfolio: ROUND(
-//               //             (value / portfolioValue) * 100
-//               //           ),
-//               //         })
-//               //         .eq("ticker", item.ticker)
-//               //         .eq("portfolio_id", item.portfolio_id);
-//               //       if (portfolio.data) {
-//               //         const portfolioCost = portfolio.data.cost;
-//               //         await supabaseClient
-//               //           .from("portfolio")
-//               //           .update({
-//               //             value: portfolioValue,
-//               //             gain_value: ROUND(portfolioValue - portfolioCost),
-//               //             gain_margin: ROUND(
-//               //               ((portfolioValue - portfolioCost) / portfolioCost) *
-//               //                 100
-//               //             ),
-//               //           })
-//               //           .eq("id", item.portfolio_id);
-//               //       }
-//               //     }
-//               //   });
-//               // }
-//             }
-//           }
-//         } catch (error) {}
-//       }, 600 * index);
-//     });
-//   }
-//   res.json({ results: stocks, route: "/update-price-current" });
-// });
-
-// const job = new cron.CronJob("*/1 * * * *", async () => {
-//   await axios.get(`${process.env.SERVER_URL}/update-price-current`);
-// });
-// job.start();
-
-app.listen(80, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("SERVER WORK")
 });
