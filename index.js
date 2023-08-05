6d46e61f31e62250c16298eec0fb6d04c51290a6
Vasyl Dubno <vasyldubno@gmail.com>
2023-08-05 12:03:58 -0700

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { supabaseClient } = require("./src/config/supabaseClient.js");
 
 require("dotenv").config();
 
@@ -12,6 +13,11 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({ message: "Ok", route: "/" });
 });
+
+app.get('/update-pricr-current', async (req, res) => {
+  const stocks = await supabaseClient.from('stock').select().order('ticker', {ascending: true})
+  res.json({ stocks })
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("SERVER WORK")
