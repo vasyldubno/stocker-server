58d71a6488d0ebdcb736badcefbfb2e262c777bf
Vasyl Dubno <vasyldubno@gmail.com>
2023-08-07 12:39:22 -0700

    fix

--- index.js
+++ index.js
@@ -28,18 +28,11 @@ app.get('/update-price-current', async (req, res) => {
   .from('stock')
   .select()
   .order('ticker', { ascending: true })
-  // .in('ticker', ['A', 'AAPL'])
 
   if (stocks.data) {
     stocks.data.forEach((stock, index) => {
       setTimeout(async () => {
         try {
-          // const stockPortfolio = await supabaseClient
-          //   .from("stock_portfolio")
-          //   .select()
-          //   .eq("ticker", stock.ticker)
-          //   .in("portfolio_id", portfolioIds ?? []);
-
           if (stock.ticker && stock.price_target) {
             const priceCurrent = await getPriceCurrent(
               stock.ticker,
@@ -64,74 +57,6 @@ app.get('/update-price-current', async (req, res) => {
                   gfValueMargin: gfValueMargin
                 })
                 .eq('ticker', stock.ticker)
-
-              // const res = await client
-              //   .from("stock")
-              //   .update({
-              //     price_current: priceCurrent,
-              //     price_growth: priceGrowth,
-              //     price_year_high:
-              //       stock.price_year_high &&
-              //       priceCurrent > stock.price_year_high
-              //         ? priceCurrent
-              //         : stock.price_year_high,
-              //     gfValueMargin: ROUND(
-              //       ((Number(stock.gfValue) - priceCurrent) / priceCurrent) *
-              //         100
-              //     ),
-              //   })
-              //   .eq("ticker", stock.ticker).select();
-
-              // if (stockPortfolio.data) {
-              //   stockPortfolio.data.forEach(async (item) => {
-              //     if (
-              //       item.amount_active_shares &&
-              //       item.average_cost_per_share
-              //     ) {
-              //       const portfolio = await supabaseClient
-              //         .from("portfolio")
-              //         .select()
-              //         .eq("id", item.portfolio_id)
-              //         .single();
-              //       const value = ROUND(
-              //         priceCurrent * item.amount_active_shares
-              //       );
-              //       const cost = ROUND(
-              //         item.average_cost_per_share * item.amount_active_shares
-              //       );
-              //       const portfolioValue = ROUND(
-              //         await getPortfolioValue(item.portfolio_id)
-              //       );
-              //       await supabaseClient
-              //         .from("stock_portfolio")
-              //         .update({
-              //           price_current: priceCurrent,
-              //           price_growth: priceGrowth,
-              //           gain_value: ROUND(value - cost),
-              //           gain_margin: ROUND(((value - cost) / cost) * 100),
-              //           perc_of_portfolio: ROUND(
-              //             (value / portfolioValue) * 100
-              //           ),
-              //         })
-              //         .eq("ticker", item.ticker)
-              //         .eq("portfolio_id", item.portfolio_id);
-              //       if (portfolio.data) {
-              //         const portfolioCost = portfolio.data.cost;
-              //         await supabaseClient
-              //           .from("portfolio")
-              //           .update({
-              //             value: portfolioValue,
-              //             gain_value: ROUND(portfolioValue - portfolioCost),
-              //             gain_margin: ROUND(
-              //               ((portfolioValue - portfolioCost) / portfolioCost) *
-              //                 100
-              //             ),
-              //           })
-              //           .eq("id", item.portfolio_id);
-              //       }
-              //     }
-              //   });
-              // }
             }
           }
         } catch (error) {}
